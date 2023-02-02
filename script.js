3e63e8bf3c637bdf0d630be5f20655f2dc6bc385
sebastian melph <jame1307@stud.kea.dk>
2023-02-02 22:24:07 +0100

    struggling

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function init () {
+    console.log("init");
+
+}
+
+
+function getRandomChoice(){
+    console.log("getRandomChoice");
+
+
+}
+
+function userChoice() {
+    console.log("userChoice");
+
+}
